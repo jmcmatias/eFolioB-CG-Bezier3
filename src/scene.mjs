85099c6302df6192a3ea2f8fb5831eb33a34bebe
Jorge Matias <jmcmatias@gmail.com>
2021-12-19 12:07:20 +0000

    Config files end, comment start

--- src/scene.mjs
+++ src/scene.mjs
@@ -4,149 +4,61 @@
 */
 
 import * as THREE from 'https://unpkg.com/three@0.124.0/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.124.0/examples/jsm/controls/OrbitControls.js'
 import { displayRaster, createSphere, createBezier3 } from './myObjects.mjs';
-import { bSelected, C0, C1, C2, C3 } from './ballsConfig.mjs'
+import { bSelected, C0, C1, C2, C3, balls } from './ballsConfig.mjs'
+import { renderer, camera, axes, raycaster, scene, lightTop, lightBot } from './sceneConfig.mjs'
 
-let renderer, scene, camera, controls;  //Variaveis para funcionamento do THREE.js 
-
-let pixels = [];              //Array que irá receber os pixeis do displayRaster
-const axisLength = 10;        //Comprimento dos eixos
-let axes = new THREE.AxesHelper(axisLength);  // inicializa os eixos com comprimento axisLength
-var balls = [];              //array que irá receber as bolas 
+// Configurações do Tabuleiro
 let rasterDisplaySize = 10;   //tamanho da grelha inicial de pixeis, poderá mudar a pedido do utilizador
 let rasterDisplayOpacity = 0.4;
+let pixels = [];              //Array que irá receber os pixeis do displayRaster
 const pixelSize = 1;          //tamanho do pixel irão manter-se sempre a 1
-const tileColor = 0xffff00;            //0xffff00 - amarelo
 const gridColor1 = 0xff8000;            //0xff8000 - laranja
 const gridColor2 = 0x408080;            //0x408080 - azulado
-//const intersectedPixelColor = 0xff0000; //0xff0000 - Vermelho
 
-/* let bSelected = {
-    selected: false,
-    ball: 0
-};
-
-// Objetos que para representar as configurações dos pontos
-let C0 = {
-    color: "yellow",
-    position: new THREE.Vector3(-4, -4, 0),
-    startPosition: new THREE.Vector3(-4, -4, 0)
-}
-
-let C1 = {
-    color: "red",
-    position: new THREE.Vector3(-4, 4, 0),
-    startPosition: new THREE.Vector3(-4, 4, 0)
-}
-
-let C2 = {
-    color: "green",
-    position: new THREE.Vector3(4, 4, 0),
-    startPosition: new THREE.Vector3(4, 4, 0)
-
-}
-
-let C3 = {
-    color: "blue",
-    position: new THREE.Vector3(4, -4, 0),
-    startPosition: new THREE.Vector3(4, -4, 0)
-
-} */
-
-const raycaster = new THREE.Raycaster(); // cria uma instancia de raycaster 
+// Vetores para utilização com o raycaster e coordenadas do rato
 let mouse = new THREE.Vector2();         // cria um vector2 para receber as posições do rato devolvidas por onMouseMove 
-let mouseIntersects = new THREE.Vector2();
+let mouseIntersects = new THREE.Vector2();  // Valor que irá receber o ponto de interseção do ray com o tabuleiro
 
 init();
 animate();
 
 function init() {
     // renderer 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
-    document.body.appendChild(renderer.domElement);
+    getRenderer();
     // scene 
-    scene = new THREE.Scene();
-
-    // camera
-    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.set(0, 0, 20);
-
-    // controls 
-    controls = new OrbitControls(camera, renderer.domElement);
-
     getLights();
-
-    const size = 20;
-    const divisions = 10;
-
-
     // Desenha os eixos 
     getAxes();
     // Desenha o Display Raster
     getDisplayRaster();
     // Desenha as bolas 
     getBalls();
-
-
-}
-// EventListener que devolve a posição do rato quando este se move com as coordenadas normalizadas e chama a função onMouseMove
-document.body.addEventListener('mousemove', onMouseMove, false);
-
-function getCamera() {
-
 }
 
+
+
+// Função que insere o renderer no DOM, criação do node canvas
+function getRenderer() {
+    document.body.appendChild(renderer.domElement);
+}
+
+// Função que insere as luzes na scene
 function getLights() {
-    // ambient 
-    const lightTop = new THREE.DirectionalLight(0xffffff, 1, 1000);
-    const lightBot = new THREE.DirectionalLight(0xffffff, 1, 1000);
-
-    lightTop.position.set(0, 0, 100); //
-    lightTop.castShadow = true;
-
-    lightBot.position.set(0, 0, -100);
-    lightBot.castShadow = true;
     scene.add(lightTop);
     scene.add(lightBot);
 }
 
-// Função que irá receber um callback com a ultima posição do rato x e y
-function onMouseMove(event) {
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-    if (bSelected.selected) {
-        intersectionPoint();
-        updateCoordenates();
-        let C = balls[bSelected.ball];
-        let newXY = new THREE.Vector3();
-        raycaster.setFromCamera(mouse, camera);                    // inicializa o raycaster com as posições do rato em relação á camara
-        const intersects = raycaster.intersectObjects(pixels);     // coloca os objetos existentes no array pixels que são intercetados pelo ray no array intersects
-        //console.log(intersects);
-        if (intersects.length > 0) {                               // Se existir alguma interseção
-            newXY = intersects[0].point;
-            C.position.x = newXY.x;
-            C.position.y = newXY.y;
-        }
 
-    }
-    // console.log(mouse);
-}
 
 function getBalls() {
+    // Insere as bolas criadas nos pontos C0,C1,C2,C3 no array balls
     balls.push(createSphere(C0));
     balls.push(createSphere(C1));
     balls.push(createSphere(C2));
     balls.push(createSphere(C3));
-
-    scene.add(balls[0]);
-    scene.add(balls[1]);
-    scene.add(balls[2]);
-    scene.add(balls[3]);
+    // Insere as bolas na scene
+    balls.every (ball => scene.add(ball));
 }
 
 //Função que insere o display raster na scene
@@ -157,11 +69,11 @@ function getDisplayRaster() {
 
 // Função que vai inserir os eixos na scene
 function getAxes() {
-    axes = new THREE.AxesHelper(axisLength);  // inicializa os eixos com comprimento axisLength
     axes.position.z = 0.05      // coloca os eixos um pouco acima do display raster para que se vejam bem
     scene.add(axes);            // adiciona na scene
 }
 
+// Função que recebe um número e seleciona a bola que corresponde a esse numero 
 function ballSelected(i) {
     bSelected.selected = true;
     bSelected.ball = i;
@@ -203,9 +115,10 @@ function showCoordenates() {
 
 }
 
+// Atualiza as coordenadas da bola selecionada no ecrã
 function updateCoordenates() {
-    let C = balls[bSelected.ball];
-    if (document.getElementById('coord')) {
+    const C = balls[bSelected.ball];            // inicializa C com a bola selecionada
+    if (document.getElementById('coord')) {     // Se existir o div coord insere as coordenadas no seu innerHTML com os valores arredondados a duas casas decimais, em que x,y é o ponto de interceção do rato com o tabuleiro e z a coordenada z atual da bola 
         document.getElementById('coord').innerHTML = 'Coordenadas de C' + bSelected.ball + ' (' + mouseIntersects.x.toFixed(2) + ',' + mouseIntersects.y.toFixed(2) + ',' + C.position.z.toFixed(2) + ')';
     }
 }
@@ -218,52 +131,15 @@ function removeCoordenates() {
 }
 
 function intersectionPoint() {
-    raycaster.setFromCamera(mouse, camera);                    // inicializa o raycaster com as posições do rato em relação á camara
+    raycaster.setFromCamera(mouse, camera);                    // inicializa o raycaster com as posições do rato em relação á camera
     const intersects = raycaster.intersectObjects(pixels);     // coloca os objetos existentes no array pixels que são intercetados pelo ray no array intersects
     if (intersects.length > 0) {                               // Se existir alguma interseção
-        mouseIntersects = intersects[0].point;
+        mouseIntersects = intersects[0].point;                 // Guarda o ponto em mouseIntersects
     }
 }
 
 
-// EventListener que "escuta por teclas pressionadas" e chama a função keydown
-document.body.addEventListener('keydown', onKeyDown, false);
 
-// Função que recebe o callback do evento keydown
-function onKeyDown(event) {
-    const keyName = event.key;              // inicializa keyName com o nome da tecla pressionada
-    let i = 0;
-    //console.log(keyName); // DEBUG only
-    if (keyName == 'Backspace') {           // caso seja Backspace
-        resetScene();                       // Faz reset a scene
-    }
-
-    if (bSelected.selected == false && (keyName == '1' || keyName == '2' || keyName == '3' || keyName == '4')) {
-        ballSelected(parseInt(keyName) - 1);
-    }
-
-    if (bSelected.selected == true) {
-        if (event.keyCode == 32){      // Caso a tecla pressionada seja o space e esteja uma bola seleccionada
-            ballDeselected();
-        }
-        if (keyName == 'w') {
-            balls[bSelected.ball].position.z = balls[bSelected.ball].position.z + 0.1;
-            updateCoordenates();
-        }
-
-        if (keyName == 's') {
-            balls[bSelected.ball].position.z = balls[bSelected.ball].position.z - 0.1;
-            updateCoordenates();
-        }
-
-    }
-
-    if (keyName == 'x') {               // se a tecla pressionada for 'x'       
-        let bezierCurve3 = createBezier3(balls);
-        scene.add(bezierCurve3);
-
-    }
-}
 
 
 
@@ -329,6 +205,73 @@ function animate() {
     window.requestAnimationFrame(animate);
 }
 
+//////////////////////////////////////////
+// EVENT HANDLERS E FUNÇÕES DEPENDENTES //
+//////////////////////////////////////////
+
+// EventListener que devolve a posição do rato quando este se move com as coordenadas normalizadas e chama a função onMouseMove
+document.body.addEventListener('mousemove', onMouseMove, false);
+
+// Função que irá receber um callback com a ultima posição do rato x e y
+function onMouseMove(event) {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+    if (bSelected.selected) {                                   // Caso esteja alguma bola selecionada
+        intersectionPoint();                                    // Atualiza o ponto de Interseção do raycaster com o tabuleiro
+        updateCoordenates();                                    // Atualiza as coordenadas de modo a aparecerem no ecrã ao mover-se o rato por cima do tabuleiro
+        let C = balls[bSelected.ball];                          // coloca em C a bola selecionada
+        let newXY = new THREE.Vector2();                        // cria um vector 2 para receber as coordenadas da interceção do rato com o tabuleiro
+        raycaster.setFromCamera(mouse, camera);                    // inicializa o raycaster com as posições do rato em relação á camara
+        const intersects = raycaster.intersectObjects(pixels);     // Põe em intersects a informação da interceção do rato com objetos contidos em pixels (tabuleiro)
+        //console.log(intersects);       // DEBUG only
+        if (intersects.length > 0) {                               // Se existir alguma interseção
+            newXY = intersects[0].point;                           // coloca as coordenadas x,y do ponto de interseção em newXY
+            C.position.x = newXY.x;                                // atualiza a coordenada x da bola selecionada com a do ponto de interseção
+            C.position.y = newXY.y;                                // atualiza a coordenada x da bola selecionada com a do ponto de interseção
+        }
+    }
+    // console.log(mouse); // DEBUG only
+}
+
+// EventListener que "escuta por teclas pressionadas" e chama a função keydown
+document.body.addEventListener('keydown', onKeyDown, false);
+
+// Função que recebe o callback do evento keydown
+function onKeyDown(event) {
+    const keyName = event.key;              // inicializa keyName com o nome da tecla pressionada
+    let i = 0;
+    //console.log(keyName); // DEBUG only
+    if (keyName == 'Backspace') {           // caso seja Backspace
+        resetScene();                       // Faz reset a scene
+    }
+
+    if (bSelected.selected == false && (keyName == '1' || keyName == '2' || keyName == '3' || keyName == '4')) {
+        ballSelected(parseInt(keyName) - 1);
+    }
+
+    if (bSelected.selected == true) {
+        if (event.keyCode == 32){      // Caso a tecla pressionada seja o space e esteja uma bola seleccionada
+            ballDeselected();
+        }
+        if (keyName == 'w') {
+            balls[bSelected.ball].position.z = balls[bSelected.ball].position.z + 0.1;
+            updateCoordenates();
+        }
+
+        if (keyName == 's') {
+            balls[bSelected.ball].position.z = balls[bSelected.ball].position.z - 0.1;
+            updateCoordenates();
+        }
+
+    }
+
+    if (keyName == 'x') {               // se a tecla pressionada for 'x'       
+        let bezierCurve3 = createBezier3(balls);
+        scene.add(bezierCurve3);
+
+    }
+}
+
 // Event Listener que deteta se a janela foi redimensionada e chama a função resize
 window.addEventListener("resize", resize, false);
 
@@ -339,6 +282,7 @@ function resize() {
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
+
 
 export default scene;   // exporta a scene
 
